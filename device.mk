76f3039f8b56dc73cf8d0050c5ecad7abee616b8
Khalvat-M <mmba313@gmail.com>
2019-07-29 15:34:31 +0430

    Add AOSP overlay

--- device.mk
+++ device.mk
@@ -25,6 +25,7 @@ $(call inherit-product-if-exists, vendor/samsung/ks01ltexx/ks01ltexx-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-aosp
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
 # Device uses high-density artwork where available
@@ -65,6 +66,10 @@ PRODUCT_PACKAGES += \
 # FlipFlap
 PRODUCT_PACKAGES += \
     FlipFlap
+
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
 
 # Input device
 PRODUCT_COPY_FILES += \
