22b87be05dc900b5361661f1dfb561786b2ff0e7
Khalvat-M <mmba313@gmail.com>
2019-07-31 19:02:04 +0430

    Add qcom privapp permissions
    * https://source.android.com/devices/tech/config/perms-whitelist#generating-whitelists

--- device.mk
+++ device.mk
@@ -115,6 +115,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
     $(LOCAL_PATH)/configs/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml
 
+# Privapp Whitelist
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml
+
 # Radio
 PRODUCT_PACKAGES += \
     libsecnativefeature
