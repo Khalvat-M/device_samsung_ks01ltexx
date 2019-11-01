a67b2b0208f011d29765073d126db53dd5f1e25f
MBA <44835312+Khalvat-M@users.noreply.github.com>
2019-11-01 11:15:32 +0330

    Enforce RRO for all resource overlays

--- device.mk
+++ device.mk
@@ -25,6 +25,8 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-aosp
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
+PRODUCT_ENFORCE_RRO_TARGETS := *
+
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
