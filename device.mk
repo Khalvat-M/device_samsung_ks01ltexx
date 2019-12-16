c2eb01b76b67580becde4c46b22bfa79e70ddcb7
MBA <44835312+Khalvat-M@users.noreply.github.com>
2019-12-16 04:40:35 +0330

    Reactivation samsung doze for none aosp builds
    * Enable doze for lineage and lineage base roms

--- device.mk
+++ device.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2014 The CyanogenMod Project
+# Copyright (C) 2019 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -155,8 +155,8 @@ PRODUCT_PACKAGES += \
     liboverlay
 
 # Doze
-#PRODUCT_PACKAGES += \
-#    SamsungDoze
+PRODUCT_PACKAGES += \
+    SamsungDoze
 
 # DRM
 PRODUCT_PACKAGES += \
