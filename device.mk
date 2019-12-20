b591e78e23289961f65e0ebf68770fbd01f3996c
Khalvat-M <mmba313@gmail.com>
2019-12-20 18:26:56 +0330

    Enable Exclude AudioFX

--- device.mk
+++ device.mk
@@ -116,6 +116,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# Exclude AudioFX
+TARGET_EXCLUDES_AUDIOFX := true
+
 # Bluetooth: prepatch to fix BT/WiFi bus lockups
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
