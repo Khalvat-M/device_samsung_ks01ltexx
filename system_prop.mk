97ce219190051860f48d497bcd614d4d1349623d
Khalvat-M <mmba313@gmail.com>
2020-01-04 13:47:35 +0330

    fix rank of OMX audio codec components
    Rank OMX SW audio codecs higher than C2 codecs.

--- system_prop.mk
+++ system_prop.mk
@@ -18,6 +18,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Audio (Media)
 PRODUCT_PROPERTY_OVERRIDES += \
+    debug.stagefright.omx_default_rank.sw-audio=1 \
+    debug.stagefright.omx_default_rank=0 \
     audio.deep_buffer.media=true \
     audio.offload.disable=true \
     audio.offload.pcm.16bit.enable=true \
