0efe3ca51d3f0215465a1ef61aa4571972a6abb8
Alessandro Astone <ales.astone@gmail.com>
2020-03-07 19:00:19 +0100

    Build and enable fastbootd

--- ks01lte.mk
+++ ks01lte.mk
@@ -188,6 +188,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/izat.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/izat.conf \
     $(LOCAL_PATH)/gps/etc/sap.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/sap.conf
 
+# fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+    
 # Freeform Multiwindow
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
