ca7b282bf7580bd2f4684235398c25ba86435676
Khalvat-M <mmba313@gmail.com>
2020-04-27 06:08:04 +0430

    Drop deprecated flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -129,10 +129,6 @@ TARGET_PROCESS_SDK_VERSION_OVERRIDE += \
     /system/vendor/bin/mm-qcamera-daemon=22 \
     /system/vendor/bin/hw/rild=27
 
-
-# LineageHW
-BOARD_HARDWARE_CLASS += hardware/samsung/lineagehw
-
 # NFC
 include $(DEVICE_PATH)/nfc/bcm2079x/board.mk
 
