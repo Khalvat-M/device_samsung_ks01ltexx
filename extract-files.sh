Khalvat-M/device_samsung_msm8974-common

Update and Fixup device *.sh files from the device template

extract-files.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2020 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -22,54 +21,47 @@ set -e
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-CM_ROOT="$MY_DIR"/../../..
+LINEAGE_ROOT="$MY_DIR"/../../..
 
-HELPER="$CM_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
 fi
 . "$HELPER"
 
-if [ $# -eq 0 ]; then
+# Default to sanitizing the vendor folder before extraction
+CLEAN_VENDOR=true
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -n | --no-cleanup )     CLEAN_VENDOR=false
+                                ;;
+        -s | --section )        shift
+                                SECTION=$1
+                                CLEAN_VENDOR=false
+                                ;;
+        * )                     SRC=$1
+                                ;;
+    esac
+    shift
+done
+
+if [ -z "$SRC" ]; then
     SRC=adb
-else
-    if [ $# -eq 1 ]; then
-        SRC=$1
-    else
-        echo "$0: bad number of arguments"
-        echo ""
-        echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
-        echo ""
-        echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
-        echo "the device using adb pull."
-        exit 1
-    fi
 fi
 
 # Initialize the helper for common device
-setup_vendor "$DEVICE_COMMON" "$VENDOR" "$CM_ROOT" true
+setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true "$CLEAN_VENDOR"
 
-extract "$MY_DIR"/common-proprietary-files.txt "$SRC"
-extract "$MY_DIR"/common-proprietary-files-pn547.txt "$SRC"
+extract "$MY_DIR"/common-proprietary-files.txt "$SRC" "$SECTION"
+extract "$MY_DIR"/common-proprietary-files-pn547.txt "$SRC" "$SECTION"
 
-COMMON_BLOB_ROOT="$CM_ROOT"/vendor/"$VENDOR"/"$DEVICE_COMMON"/proprietary
+if [ -s "$MY_DIR"/../$DEVICE/device-proprietary-files.txt ]; then
+    # Reinitialize the helper for device
+    setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false "$CLEAN_VENDOR"
 
-MMCAMERA2_SENSOR_MODULES="$COMMON_BLOB_ROOT"/vendor/lib/libmmcamera2_sensor_modules.so
-sed -i 's|system/etc|vendor/etc|g;
-        s|/system/lib|/vendor/lib|g;
-        s|/system/cameradata|/vendor/cameradata|g' "$MMCAMERA2_SENSOR_MODULES"
-
-THERMAL_ENGINE="$COMMON_BLOB_ROOT"/vendor/bin/thermal-engine
-sed -i 's|/system/etc|/vendor/etc|g' "$THERMAL_ENGINE"
-
-# Reinitialize the helper for device
-setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
-
-for BLOB_LIST in "$MY_DIR"/../$DEVICE/device-proprietary-files*.txt; do
-    extract $BLOB_LIST "$SRC"
-done
-
-./../msm8974-common/extract-files.sh $@
+    extract "$MY_DIR"/../$DEVICE/device-proprietary-files.txt "$SRC" "$SECTION"
+fi
 
 "$MY_DIR"/setup-makefiles.sh

setup-makefiles.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2020 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,45 +17,44 @@
 
 set -e
 
-export INITIAL_COPYRIGHT_YEAR=2014
+INITIAL_COPYRIGHT_YEAR=2014
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-CM_ROOT="$MY_DIR"/../../..
+LINEAGE_ROOT="$MY_DIR"/../../..
 
-HELPER="$CM_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
 fi
 . "$HELPER"
 
-# Initialize the helper for common device
-setup_vendor "$DEVICE_COMMON" "$VENDOR" "$CM_ROOT" true
+# Initialize the helper for common
+setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true
 
 # Copyright headers and common guards
 write_headers "klte klteactivexx kltechn kltechnduo klteduos kltedv kltekdi kltekor kltespr kltesprsports klteusc kltevzw"
 
-write_makefiles "$MY_DIR"/common-proprietary-files.txt
+# The standard common blobs
+write_makefiles "$MY_DIR"/common-proprietary-files.txt true
 
+# We are done!
 write_footers
 
-if [ ! -z $VARIANT_COPYRIGHT_YEAR ]; then
-    export INITIAL_COPYRIGHT_YEAR=$VARIANT_COPYRIGHT_YEAR
+if [ -s "$MY_DIR"/../$DEVICE/proprietary-files.txt ]; then
+    # Reinitialize the helper for device
+    INITIAL_COPYRIGHT_YEAR="$DEVICE_BRINGUP_YEAR"
+    setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false
+
+    # Copyright headers and guards
+    write_headers
+
+    # The standard device blobs
+    write_makefiles "$MY_DIR"/../$DEVICE/device-proprietary-files.txt true
+
+    # We are done!
+    write_footers
 fi
-
-# Reinitialize the helper for device
-setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
-
-# Copyright headers and guards
-write_headers
-
-for BLOB_LIST in "$MY_DIR"/../$DEVICE/device-proprietary-files*.txt; do
-    write_makefiles $BLOB_LIST
-done
-
-write_footers
-
-./../msm8974-common/setup-makefiles.sh $@

Khalvat-M/device_samsung_ks01ltexx

extract-files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2020 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,63 +17,55 @@
 
 set -e
 
-DEVICE=ks01ltexx
-VENDOR=samsung
+export DEVICE=ks01ltexx
+export VENDOR=samsung
+export DEVICE_BRINGUP_YEAR=2013
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-CM_ROOT="$MY_DIR"/../../..
+LINEAGE_ROOT="$MY_DIR"/../../..
 
-HELPER="$CM_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
 fi
 . "$HELPER"
 
-if [ $# -eq 0 ]; then
+# Default to sanitizing the vendor folder before extraction
+CLEAN_VENDOR=true
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -n | --no-cleanup )     CLEAN_VENDOR=false
+                                ;;
+        -s | --section )        shift
+                                SECTION=$1
+                                CLEAN_VENDOR=false
+                                ;;
+        * )                     SRC=$1
+                                ;;
+    esac
+    shift
+done
+
+if [ -z "$SRC" ]; then
     SRC=adb
-else
-    if [ $# -eq 1 ]; then
-        SRC=$1
-    else
-        echo "$0: bad number of arguments"
-        echo ""
-        echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
-        echo ""
-        echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
-        echo "the device using adb pull."
-        exit 1
-    fi
 fi
 
 # Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT" true
+setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" true "$CLEAN_VENDOR"
 
-extract "$MY_DIR"/proprietary-files.txt "$SRC"
-extract "$MY_DIR"/proprietary-files-twrp.txt "$SRC"
+extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
+extract "$MY_DIR"/proprietary-files-twrp.txt "$SRC" "$SECTION"
 
-DEVICE_BLOB_ROOT="$CM_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
+if [ -s "$MY_DIR"/../$DEVICE/proprietary-files.txt ]; then
+    # Reinitialize the helper for device
+    setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false "$CLEAN_VENDOR"
 
-MMCAMERA2_SENSOR_MODULES="$DEVICE_BLOB_ROOT"/vendor/lib/libmmcamera2_sensor_modules.so
-sed -i 's|system/etc|vendor/etc|g;
-        s|/system/lib|/vendor/lib|g;
-        s|/system/cameradata|/vendor/cameradata|g' "$MMCAMERA2_SENSOR_MODULES"
-
-THERMAL_ENGINE="$DEVICE_BLOB_ROOT"/vendor/bin/thermal-engine
-sed -i 's|/system/etc|/vendor/etc|g' "$THERMAL_ENGINE"
-
-# Reinitialize the helper for device
-setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
-
-for BLOB_LIST in "$MY_DIR"/../$DEVICE/device-proprietary-files*.txt; do
-    extract $BLOB_LIST "$SRC"
-done
-
-./../msm8974-common/extract-files.sh $@
-
-"$MY_DIR"/setup-makefiles.sh
+    extract "$MY_DIR"/../$DEVICE/proprietary-files.txt "$SRC" "$SECTION"
+fi
 
 "$MY_DIR"/setup-makefiles.sh

setup-makefiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (C) 2019 The CyanogenMod Project
+# Copyright (C) 2020 The Lineage OS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,31 +17,38 @@
 
 set -e
 
-export INITIAL_COPYRIGHT_YEAR=2019
+# Required!
+export DEVICE=ks01ltexx
+export VENDOR=samsung
+export DEVICE_BRINGUP_YEAR=2013
 
-DEVICE=ks01ltexx
-VENDOR=samsung
+
+INITIAL_COPYRIGHT_YEAR=2013
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-CM_ROOT="$MY_DIR"/../../..
+LINEAGE_ROOT="$MY_DIR"/../../..
 
-HELPER="$CM_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
 fi
 . "$HELPER"
 
-# Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT" true
+# Initialize the helper for device
+setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" true
 
 # Copyright headers and guards
-write_headers
+write_headers "ks01ltexx ks01lteskt ks01ltektt ks01ltelgt"
 
-write_makefiles "$MY_DIR"/proprietary-files.txt
+# The standard device blobs
+write_makefiles "$MY_DIR"/proprietary-files.txt true
+
+# We are done!
+write_footers
 
 # Blobs for TWRP data decryption
 cat << EOF >> "$BOARDMK"
@@ -53,17 +60,19 @@ EOF
 # Finish
 write_footers
 
-if [ ! -z $VARIANT_COPYRIGHT_YEAR ]; then
-    export INITIAL_COPYRIGHT_YEAR=$VARIANT_COPYRIGHT_YEAR
+if [ -s "$MY_DIR"/../$DEVICE/proprietary-files.txt ]; then
+    # Reinitialize the helper for device
+    INITIAL_COPYRIGHT_YEAR="$DEVICE_BRINGUP_YEAR"
+    setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false
+
+    # Copyright headers and guards
+    write_headers
+
+    # The standard device blobs
+    write_makefiles "$MY_DIR"/../$DEVICE/proprietary-files.txt true
+
+    # We are done!
+    write_footers
 fi
 
-# Reinitialize the helper for device
-setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
-
-for BLOB_LIST in "$MY_DIR"/../$DEVICE/device-proprietary-files*.txt; do
-    write_makefiles $BLOB_LIST
-done
-
-write_footers
-
-./../msm8974-common/setup-makefiles.sh $@
+./../../$VENDOR/$DEVICE/setup-makefiles.sh $@