60e3c7e8f6fff7401224ad2a70e6660d541589e2
Khalvat-M <mmba313@gmail.com>
2020-07-14 20:14:41 +0430

    Nuke camera config

--- ks01lte.mk
+++ ks01lte.mk
@@ -139,9 +139,6 @@ PRODUCT_PACKAGES += \
     libxml2 \
     Snap
    
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
-
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
