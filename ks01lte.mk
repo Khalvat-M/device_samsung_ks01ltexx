7fa0d60871e36cbadc711c731a7927111949bb3e
Khalvat-M <mmba313@gmail.com>
2020-09-27 19:50:49 +0330

    Switch to new Power Hal
    Signed-off-by: Mohammad(Khalvat-M)<mmba313@gmail.com>

--- ks01lte.mk
+++ ks01lte.mk
@@ -262,7 +262,7 @@ PRODUCT_PACKAGES += \
 
 # Power HAL
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
+    android.hardware.power-service-qti
 
 # Privapp Whitelist
 PRODUCT_COPY_FILES += \
